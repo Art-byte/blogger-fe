f7f527f9e7a7fc843910e79110da6b9b86dd3148
Art-byte <arturo_pdrzbn@hotmail.com>
2024-12-13 16:12:19 -0600

    style: Se crea la parte visual para el componente de creacion de blog

--- src/app/home/blog/create-blog/create-blog.component.ts
+++ src/app/home/blog/create-blog/create-blog.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {ComponentsModule} from "../../../components.module";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-create-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentsModule,
+    RouterLink
+  ],
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.scss'
 })
 export class CreateBlogComponent {
+  blog = {
+    title: '',
+    content: '',
+  };
 
+  files: File[] = [];
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input?.files) {
+      this.files = Array.from(input.files);
+    }
+  }
+
+  triggerFileInput(): void {
+    const fileInput = document.getElementById('file-upload') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  onSubmit(): void {
+    console.log('Formulario enviado:', this.blog);
+    console.log('Archivos seleccionados:', this.files);
+    // Aquí puedes enviar los datos al backend o realizar otras acciones
+  }
 }
