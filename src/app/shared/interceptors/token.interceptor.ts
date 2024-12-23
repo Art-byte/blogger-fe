fdbd23dbec54e4386403d82b7f8c157bbed7263e
Art-byte <arturo_pdrzbn@hotmail.com>
2024-12-23 02:37:14 -0600

    feat: Se cambia agrega validacion a token interceptor para redirigir al login en caso de que falte token de session

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -1,16 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import {inject} from "@angular/core";
 import {Router} from "@angular/router";
+import {AuthService} from "../service/auth.service";
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
 
   const router = inject(Router);
+  const authService = inject(AuthService);
   const currentURL = router.url;
   if(currentURL == '/' || currentURL.startsWith('login')){
     return next(req);
   }
 
   const tokenSession = sessionStorage.getItem("token");
+  if(tokenSession == null){
+    authService.logout();
+  }
+
   if(tokenSession){
     const authReq = req.clone({
       headers: req.headers.set("Authorization", `Bearer ${tokenSession}`)
