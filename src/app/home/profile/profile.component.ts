9884b3283b482d866e65b5debfb84b02e45ff5a6
Art-byte <arturo_pdrzbn@hotmail.com>
2025-01-01 18:06:35 -0600

    feat: Se crea el codigo para el formulario de perfil de usuario

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -1,20 +1,35 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {UserService} from "../../shared/service/user.service";
 import {AuthService} from "../../shared/service/auth.service";
+import {ComponentsModule} from "../../components.module";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit{
 
+  reactiveForm: FormGroup;
+  private readonly profileForm = inject(FormBuilder);
   private readonly authService = inject(AuthService);
   private readonly userService = inject(UserService);
 
+  initForm(): void{
+    this.reactiveForm = this.profileForm.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      aboutMe: ['', Validators.required],
+    });
+  }
+
   ngOnInit(): void {
+    this.initForm();
     this.getDataFromUser();
   }
 
